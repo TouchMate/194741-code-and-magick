2e87c04d4752e23cd5e7e6cb63a92ec1c00f843f
Alexander Sakaev <sakaev11alexander@gmail.com>
2017-08-15 02:56:08 +0400

    separated function for each action

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
-'use strick';
+'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  console.log(names);
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -11,42 +11,54 @@ window.renderStatistics = function (ctx, names, times) {
 
 
   ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
+  ctx.font = '16px PT Mono,';
+
 
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов', 120, 60);
-  var max = -1;
 
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
+  var max = -1;
+  function findMaxValue() {
+    for (var i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+      }
     }
   }
+  findMaxValue(max);
+
+
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  var step = histogramHeight / (max);
   ctx.textBaseline = 'bottom';
-  var indent = 50; // px;
+  var indent = 100; // px;
   var barWidth = 40; // px;
   var initialX = 120; // px;
   var initialY = 240; // px;
   var playerColor = 'rgba(255, 0, 0, 1)';
 
-  for (var i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = playerColor;
-    } else {
-      ctx.fillStyle = 'hsl(200, 99%, ' + Math.random() * 100 + '%)';
-    }
-    function compareNumeric(a, b) {
-      if (a > b) return 1;
-      if (a < b) return -1;
-    }
-    times.sort(compareNumeric);
-
+  function getRandomColor() {
+    return 'hsl(200, 99%, ' + Math.random() * 100 + '%)';
+  }
+  function assingColorToColumn(i) {
+    ctx.fillStyle = names[i] === 'Вы' ? playerColor : getRandomColor();
+  }
+  function buildGameChart(i) {
     ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
+  }
+  function addChartNames(i) {
     ctx.fillText(names[i], initialX + indent * i, initialY + 20);
+  }
+  function addChartNumbers(i) {
     ctx.fillText(Math.floor(times[i]), initialX + indent * i, initialY - times[i] * step);
+  }
+
+  for (var i = 0; i < times.length; i++) {
+    assingColorToColumn(i);
+    buildGameChart(i);
+    addChartNames(i);
+    addChartNumbers(i);
   }
 };
 

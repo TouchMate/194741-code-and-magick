44eee044fa7f9dd34293548aee4549d175bd721b
Alexander Sakaev <sakaev11alexander@gmail.com>
2017-08-17 00:57:08 +0400

    add randomColor to crateGameChart

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,9 @@
 'use strict';
-function getRandomColor() {
-  return 'hsl(200, 99%, ' + Math.random() * 100 + '%)';
-}
 
 function drawCloud(ctx, color, positionX, positionY) {
   ctx.fillStyle = color;
   ctx.fillRect(positionX, positionY, 420, 270);
 }
-
 
 function getMaxTime(times) {
   var amountOfPlayers = times.length;
@@ -21,7 +17,7 @@ function getMaxTime(times) {
   return max;
 }
 
-function creatGameChart(times, names, ctx) {
+function createGameChart(times, names, ctx) {
 
   var histogramHeight = 150;
   var step = histogramHeight / getMaxTime(times);
@@ -35,31 +31,19 @@ function creatGameChart(times, names, ctx) {
     ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
     ctx.fillText(names[i], initialX + indent * i, initialY + 20);
     ctx.fillText(Math.floor(times[i]), initialX + indent * i, initialY - times[i] * step);
-  }
-}
-function getColorForChart(ctx, names) {
-  var playerColor = 'rgba(255, 0, 0, 1)';
-  var amountOfPlayers = names.length;
-  for (var i = 0; i < amountOfPlayers; i++) {
-    ctx.fillStyle = names[i] === 'Вы' ? playerColor : getRandomColor();
+    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(200, 99%, ' + Math.random() * 100 + '%)';
   }
 }
 
 window.renderStatistics = function (ctx, names, times) {
   drawCloud(ctx, 'rgba(0, 0, 0, 0.7)', 110, 20);
   drawCloud(ctx, 'rgba(256, 256, 256, 1.0)', 100, 10);
-
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono,';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов', 120, 60);
-
-
   getMaxTime(times);
-  creatGameChart(times, names, ctx);
-  getColorForChart(ctx, names);
-
-
+  createGameChart(times, names, ctx);
 };
 
 

ca972e4a60cbc9df2485c823c099b0ec05b293b1
Alexander Sakaev <sakaev11alexander@gmail.com>
2017-08-17 01:22:39 +0400

    simplifed code

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,13 @@
 'use strict';
 
+
+function createWinnerMessage(ctx) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono,';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов', 120, 60);
+}
+
 function drawCloud(ctx, color, positionX, positionY) {
   ctx.fillStyle = color;
   ctx.fillRect(positionX, positionY, 420, 270);
@@ -26,22 +34,22 @@ function createGameChart(times, names, ctx) {
   var barWidth = 40; // px;
   var initialX = 120; // px;
   var initialY = 240; // px;
+
   var numberOfPlayers = times.length;
   for (var i = 0; i < numberOfPlayers; i++) {
-    ctx.fillRect(initialX + indent * i, initialY - times[i] * step, barWidth, times[i] * step);
-    ctx.fillText(names[i], initialX + indent * i, initialY + 20);
-    ctx.fillText(Math.floor(times[i]), initialX + indent * i, initialY - times[i] * step);
+    var widthBetweenCol = initialX + indent * i;
+    var heigthBetweenRow = initialY - times[i] * step;
     ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(200, 99%, ' + Math.random() * 100 + '%)';
+    ctx.fillRect(widthBetweenCol, heigthBetweenRow, barWidth, times[i] * step);
+    ctx.fillText(names[i], widthBetweenCol, initialY + 20);
+    ctx.fillText(Math.floor(times[i]), widthBetweenCol, heigthBetweenRow);
   }
 }
 
 window.renderStatistics = function (ctx, names, times) {
   drawCloud(ctx, 'rgba(0, 0, 0, 0.7)', 110, 20);
   drawCloud(ctx, 'rgba(256, 256, 256, 1.0)', 100, 10);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono,';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов', 120, 60);
+  createWinnerMessage(ctx);
   getMaxTime(times);
   createGameChart(times, names, ctx);
 };
